146376535638eba5f446d2bf12319542bfc661b7
Fabiano Lira <fabiano.l.lima@aluno.senai.br>
2026-08-12 21:15:34 -0300

    Criação do projeto

---
+++ src/main/java/com/example/imagempecas/ImagempecasApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagempecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagempecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagempecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imagempecas/ImagempecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagempecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagempecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
